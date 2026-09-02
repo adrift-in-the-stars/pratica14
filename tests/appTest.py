3280074a691633f523028a5877b2bcf0656c1eb8
CARLOS EDUARDO VASCONCELOS DE SOUZA <023.920120@universidadesenaicimatec.edu.br>
2026-09-02 19:02:05 -0300

    adicionando testes

--- tests/appTest.py
+++ tests/appTest.py
@@ -10,3 +10,12 @@ class AppTest(unittest.TestCase):
         self.assertEqual(200, response.status_code, "Erro no test_http_code!")
         self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
                           , "Erro no test_print_health_check!")
+
+    def test_print_hello_success(self):
+        response = self.app.get('/name')
+        self.assertEqual(404, response.status_code, "Erro no test_http_code!")
+
+    def test_print_hello_error (self):
+        response = self.app.get('/name')
+        self.assertNotEqual("Nome não informado", response.get_data(as_text=True)
+                          , "Erro no test_print_hello!")
